0beb6ca4138e468608a8891ec10c11ac64ba1f27
DenZaiyy <denzaiyy@outlook.fr>
2023-03-29 09:00:34 +0200

    Update forum.sql

--- ressources/db/forum.sql
+++ ressources/db/forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db:3306
--- Généré le : ven. 24 mars 2023 à 15:22
+-- Généré le : mer. 29 mars 2023 à 06:58
 -- Version du serveur : 8.0.30
 -- Version de PHP : 8.1.15
 
@@ -51,18 +51,27 @@ INSERT INTO `category` (`id_category`, `label`) VALUES
 
 CREATE TABLE `message` (
   `id_message` int NOT NULL,
-  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `topic_id` int NOT NULL,
-  `user_id` int DEFAULT NULL
+  `user_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `message`
 --
 
-INSERT INTO `message` (`id_message`, `text`, `date`, `topic_id`, `user_id`) VALUES
-(1, 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, sunt possimus sed molestias modi nam. Rem vero, in cumque dignissimos quis, laboriosam nulla assumenda error ipsa quam repellat eius eligendi?\r\n            Repudiandae, explicabo. Laudantium, reprehenderit similique quis, reiciendis doloremque possimus et aliquam inventore ducimus temporibus perferendis obcaecati at. Rerum ducimus vitae laborum modi pariatur at provident dicta, accusantium reiciendis error repellat!\r\n            Facilis id doloremque minus at commodi voluptatibus repellat ipsa ab possimus debitis ipsam dolore, eum maxime ea quisquam aliquid veniam harum nihil amet explicabo perspiciatis nemo quia? Dolorum, exercitationem minima?', '2023-03-22 19:38:11', 1, 1);
+INSERT INTO `message` (`id_message`, `message`, `date`, `topic_id`, `user_id`) VALUES
+(1, 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, sunt possimus sed molestias modi nam. Rem vero, in cumque dignissimos quis, laboriosam nulla assumenda error ipsa quam repellat eius eligendi?\r\n            Repudiandae, explicabo. Laudantium, reprehenderit similique quis, reiciendis doloremque possimus et aliquam inventore ducimus temporibus perferendis obcaecati at. Rerum ducimus vitae laborum modi pariatur at provident dicta, accusantium reiciendis error repellat!\r\n            Facilis id doloremque minus at commodi voluptatibus repellat ipsa ab possimus debitis ipsam dolore, eum maxime ea quisquam aliquid veniam harum nihil amet explicabo perspiciatis nemo quia? Dolorum, exercitationem minima?', '2023-03-22 19:38:11', 1, 1),
+(2, 'jlgjgjdlkgjdlgkjlkgjlkdjlgdj', '2023-03-28 07:23:42', 2, 1),
+(3, 'elzjrzlkjlkzjrlzkrjzlkjkljr', '2023-03-28 07:23:53', 3, 2),
+(4, 'japepaeaoeiaopeiapoeiaepoaieap', '2023-03-28 07:24:04', 4, 1),
+(5, 'epaepoaieapoeiaopeiapoeaiepa', '2023-03-28 07:24:14', 5, 1),
+(6, 'jdqqpdioeiapoeiaopeiepoaieapoksjsjdqlskjdqlkdjq', '2023-03-28 07:24:29', 6, 2),
+(7, 'A votre avis, que veux dire PDO?', '2023-03-28 07:24:40', 7, 2),
+(8, 'ljslfjskfljslkfsjflsjflksjflsjfsfjslfjsljfslkjslfjslfslj', '2023-03-28 08:02:04', 1, 2),
+(9, 'fjqsqsldjqldjqlkdjqkdjqlkq', '2023-03-28 08:26:09', 1, 2),
+(10, 'DONNE NOUS LA TRADUCTION', '2023-03-28 08:55:56', 7, 2);
 
 -- --------------------------------------------------------
 
@@ -73,7 +82,7 @@ INSERT INTO `message` (`id_message`, `text`, `date`, `topic_id`, `user_id`) VALU
 CREATE TABLE `topic` (
   `id_topic` int NOT NULL,
   `title` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `user_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` tinyint(1) NOT NULL,
   `category_id` int NOT NULL
@@ -85,11 +94,12 @@ CREATE TABLE `topic` (
 
 INSERT INTO `topic` (`id_topic`, `title`, `user_id`, `date`, `status`, `category_id`) VALUES
 (1, 'Ceci est un topic de test 1', 1, '2023-03-22 14:39:01', 0, 2),
-(2, 'Ceci est un topic de test 2', 1, '2023-03-23 08:36:33', 0, 1),
+(2, 'Ceci est un topic de test 2', 2, '2023-03-23 08:36:33', 0, 1),
 (3, 'Ceci est un topic de test 3', 1, '2023-03-23 10:02:35', 0, 2),
 (4, 'Ceci est un topic de test 4', 1, '2023-03-23 10:02:43', 0, 1),
 (5, 'Ceci est un topic de test 5', 1, '2023-03-23 10:02:51', 0, 1),
-(6, 'Ceci est un topic de test 6', 1, '2023-03-23 10:02:58', 0, 2);
+(6, 'Ceci est un topic de test 6', 1, '2023-03-23 10:02:58', 0, 2),
+(7, 'What is PDO ?', 1, '2023-03-28 06:49:27', 0, 5);
 
 -- --------------------------------------------------------
 
@@ -112,7 +122,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `pseudo`, `mail_adress`, `password`, `createdAt`, `avatar`, `role`) VALUES
-(1, 'denZ', 'denz@example.com', 'test123', '2023-03-22 19:57:22', NULL, NULL);
+(1, 'denZ', 'denz@example.com', 'test123', '2023-03-22 19:57:22', 'public/img/default-user.png', NULL),
+(2, 'test', 'test@test.com', 'testtesttest', '2023-03-28 06:48:42', NULL, NULL);
 
 --
 -- Index pour les tables déchargées
@@ -160,19 +171,19 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id_message` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_message` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `topic`
 --
 ALTER TABLE `topic`
-  MODIFY `id_topic` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_topic` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
