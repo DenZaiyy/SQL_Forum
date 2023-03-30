0ee8d108daceabb1412bc2ada7873fa23a118eed
DenZaiyy <denzaiyy@outlook.fr>
2023-03-30 17:50:56 +0200

    comment for not used

--- view/security/register.php
+++ view/security/register.php
@@ -1,12 +1,13 @@
 <h1>REGISTER FORM</h1>
 <?php
-var_dump($_POST);
+// var_dump($_POST);
 ?>
 <form action="index.php?ctrl=security&action=register" method="POST" enctype="multipart/form-data">
     <input type="text" name="pseudo" placeholder="pseudo" required>
     <input type="email" name="email" placeholder="email" required>
     <input type="password" name="password" placeholder="password" required>
     <input type="password" name="confirmPassword" placeholder="confirm password" required>
+    <!-- <input type="file" name="avatar"> -->
     <button type="submit">Valider</button>
 </form>
 
