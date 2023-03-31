a2774c8556c0e3e73140dbc5bf914bfe2ad23e47
DenZaiyy <denzaiyy@outlook.fr>
2023-03-31 12:22:24 +0200

    add  Comment form and update style of topics

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -6,6 +6,7 @@ use App\Session;
 use App\AbstractController;
 use App\ControllerInterface;
 use Model\Managers\CategoryManager;
+use Model\Managers\LikeManager;
 use Model\Managers\TopicManager;
 use Model\Managers\PostManager;
 use Model\Managers\UserManager;
@@ -13,18 +14,18 @@ use Model\Managers\UserManager;
 class ForumController extends AbstractController implements ControllerInterface
 {
 
-    // public function index()
-    // {
+    public function index()
+    {
 
-    //     $topicManager = new TopicManager();
+        $topicManager = new TopicManager();
 
-    //     return [
-    //         "view" => VIEW_DIR . "topic/listTopics.php",
-    //         "data" => [
-    //             "topics" => $topicManager->findAll(["date", "DESC"])
-    //         ]
-    //     ];
-    // }
+        return [
+            "view" => VIEW_DIR . "topic/listTopics.php",
+            "data" => [
+                "topics" => $topicManager->findAll(["date", "DESC"])
+            ]
+        ];
+    }
 
     //List of topics
     public function listTopics()
@@ -44,13 +45,15 @@ class ForumController extends AbstractController implements ControllerInterface
     {
         $topicManager = new TopicManager();
         $postManager = new PostManager();
+        $likeManager = new LikeManager();
 
         return [
             "view" => VIEW_DIR . "topic/detailTopic.php",
             "data" => [
                 "topic" => $topicManager->findOneById($id),
                 "firstMessage" => $postManager->findFirstById($id),
-                "findComments" => $postManager->findAllById($id)
+                "findComments" => $postManager->findAllById($id),
+                "like" => $likeManager->updateLikes($id)
             ]
         ];
     }
@@ -74,5 +77,42 @@ class ForumController extends AbstractController implements ControllerInterface
                 "topics" => $topicManager->findAllTopicsInCategory($id)
             ]
         ];
+    }
+
+    //Form for adding new comment to topic
+    public function addComment()
+    {
+        return [
+            "view" => VIEW_DIR . "topic/addComment.php"
+        ];
+    }
+
+    //Function like for topics
+    public function like()
+    {
+        $likeManager = new LikeManager();
+
+        $user = SESSION::getUser()->getId();
+
+        $topic = $_GET['id'];
+
+        $userLiked = $likeManager->findOneByPseudo($user);
+        $topicLiked = $likeManager->findOneByTopic($topic);
+
+        if (!$userLiked || !$topicLiked) {
+            $likeManager->add(
+                [
+                    "user_id" => $user,
+                    "topic_id" => $topic,
+                ]
+            );
+
+            $this->redirectTo("forum", "detailTopic", $topic);
+        } else {
+            $likeManager->deleteLike($topic, $user);
+            $this->redirectTo("forum", "detailTopic", $topic);
+        }
+
+        $likeManager->updateLikes($topic);
     }
 }
